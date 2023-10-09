07cb230d457941edc3e30215bf3bbaf56b273dc3
alexmatros <47996580+alexmatros@users.noreply.github.com>
2023-10-08 22:08:15 -0400

    Initial commit

---
+++ src/main/java/com/matros/mapsrouteoptimizer/MapsrouteoptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.matros.mapsrouteoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapsrouteoptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapsrouteoptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matros/mapsrouteoptimizer/MapsrouteoptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matros.mapsrouteoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsrouteoptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
